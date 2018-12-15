8d8ef144269fdc019cd220e4d8da665811add477
Jan Lindblom <janlindblom@fastmail.fm>
2018-12-15 14:08:08 +0200

    Seeing if this would do better as an extension to the led namespace.

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
 /**
  * Provides functions and blocks that lets you draw graphs of data.
  */
-//% color=190 weight=100 icon="\uf1ec" block="Graphs"
 //% groups=['LED matrix']
-namespace graphs {
+namespace led {
 
     //% block
     //% blockId=graphs_bar_graph_horisontal
     //% block="plot|horisontal|bar|graph|of %value|up|to %upTo|on|row %row"
-    //% row.min=0 row.max=4
-    export function barGraphHorisontal(value: number, upTo: number, row: number): void {
+    //% row.min=0 row.max=4 advanced=true
+    export function plotSingleBarGraphHorisontal(value: number, upTo: number, row: number): void {
         let mappedMax = Math.round(Math.map(value, 0, upTo, 0, 4))
         for (let x = 0; x <= mappedMax; x++) {
             led.plot(x, row)
@@ -24,8 +23,8 @@ namespace graphs {
     //% block
     //% blockId=graphs_bar_graph_vertical
     //% block="plot|vertical|bar|graph|of %value|up|to %upTo|on|column %column"
-    //% column.min=0 column.max=4
-    export function barGraphVertical(value: number, upTo: number, column: number): void {
+    //% column.min=0 column.max=4 advanced=true
+    export function plotSingleBarGraphVertical(value: number, upTo: number, column: number): void {
         let mappedMax = Math.round(Math.map(value, 0, upTo, 0, 4))
         for (let y = 0; y <= mappedMax; y++) {
             led.plot(column, 4 - y)
